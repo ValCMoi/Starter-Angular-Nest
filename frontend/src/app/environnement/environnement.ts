6284c32fe8355960f507cf745a07f34ba73da24c
ValCMoi <valentin.rouxel.pro@gmail.com>
2023-04-17 22:35:37 +0200

    rectification npm i + ajout utils service angular

---
+++ frontend/src/app/environnement/environnement.ts
@@ -0,0 +1,4 @@
+export const environnement = {
+     IP_NEST:'172.42.0.4',
+     NEST_PORT_INIT:'3000'
+}
---
+++ frontend/src/app/utils/api.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environnement } from '../environnement/environnement';
+
+console.log("Environnement is  :  ")
+console.table(environnement)
+const API_URL = environnement['IP_NEST']+':'+environnement['NEST_PORT_INIT'];
+console.log("API_URL", API_URL)
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  public get(url: String): Observable<any> {
+    return this.http.get('http://'+API_URL + '/api/' + url).pipe(map(res => res));
+  }
+}
